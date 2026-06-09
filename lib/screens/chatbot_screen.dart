b1a3305ed16bc17003a0bdb59d0621c0c00bbd02
AYUSH-222534 <anju00754@gmail.com>
2026-06-09 16:16:34 +0530

    Create lib/screens/chatbot_screen.dart

---
+++ lib/screens/chatbot_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatBotScreen extends StatelessWidget {
+  const ChatBotScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AI Chatbot"),
+      ),
+      body: const Center(
+        child: Text("Chatbot Coming Soon"),
+      ),
+    );
+  }
+}
